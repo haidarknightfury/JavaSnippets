3ec9f71ce0d32adf8163eff048b7f9bca84656a9
Haidar <haidar.dargaye@umail.uom.ac.mu>
2020-07-07 12:51:42 +0400

    Other controles

--- src/com/example/controles/ControleDAO.java
+++ src/com/example/controles/ControleDAO.java
@@ -5,27 +5,28 @@ import java.util.List;
 public abstract class ControleDAO extends Controle {
 
 	//Autowired
-	private final Repository repository;
+	private final Repository tControleRepository;
 	private final String code;
 	
 	public ControleDAO(Repository repository, String code, List<Precondition> preconditions) {
 		super(preconditions);
 		this.code = code;
-		this.repository = repository;
+		this.tControleRepository = repository;
 	}
 	
 	public ControleDAO(Repository repository, String code) {
 		this.code = code;
-		this.repository = repository;
+		this.tControleRepository = repository;
 	}
 	
 	@Override
 	public void applyControle() throws Exception{
-		setToggle(repository.findByCode(code));
+		setToggle(tControleRepository.findByCode(code));
 		super.applyControle();
 	}
 	
-	@Override
-	public abstract void controle() throws Exception;
+
+
+	
 
 }
---
+++ src/com/example/snippets/tryouts/test1/Snippet.java
@@ -0,0 +1,6 @@
+package com.example.snippets.tryouts.test1;
+
+public class Snippet {
+
+}
+
